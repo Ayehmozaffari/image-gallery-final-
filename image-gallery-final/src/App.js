5f78784f672e532ddc66bb12b25debbf84e77082
Ayeh <119679662+Ayehmozaffari@users.noreply.github.com>
2024-07-21 21:39:57 +0430

    changes for better imgs

--- image-gallery-final/src/App.js
+++ image-gallery-final/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Gallery from './Gallery';
 function App() {
   return (
     <>
     <h1 style={{textAlign:'center',color:'white'}}>Image gallery</h1>
     <h2 style={{textAlign:'center',color:'white'}}>enjoy</h2>
+    <Gallery/>
     </>
     
    
